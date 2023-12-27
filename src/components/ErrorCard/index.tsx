00da72ce7580632cd5743a8015626204e27f6776
Matheus P Agostinho <contact@matheuspa.com>
2023-12-26 23:50:35 -0300

    feat: added feature for remove error

--- src/components/ErrorCard/index.tsx
+++ src/components/ErrorCard/index.tsx
@@ -1,5 +1,8 @@
 import { cn } from "@/utils/cn";
 import { CopyButton } from "../CopyButton";
+import { DeleteItemButton } from "..";
+import { useModList } from "@/contexts/ModListContext";
+import { ChangeEvent, useState } from "react";
 
 interface ErrorCardProps {
   title: string;
@@ -8,13 +11,47 @@ interface ErrorCardProps {
 }
 
 export function ErrorCard({ title, description, id }: ErrorCardProps) {
+  const {
+    selectedMods,
+    removeMods,
+    removeModToSelectedList,
+    addModToSelectedList,
+  } = useModList();
+  const [cardSelected, setCardSelected] = useState(false);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setCardSelected(e.target.checked);
+    if (e.target.checked) {
+      addModToSelectedList(id);
+    } else {
+      removeModToSelectedList(id);
+    }
+  };
+
   return (
     <div
       className={cn(
-        "border border-gray-50 rounded p-4 w-full flex flex-col gap-4 shadow bg-gray-950 items-start text-gray-50",
+        "border border-gray-50 rounded p-4 w-full flex flex-col gap-4 shadow bg-gray-950 items-start text-gray-50 relative",
       )}
     >
       <div className="flex flex-col gap-4 items-start">
+        <div className="absolute top-4 right-4 flex flex-col gap-4 items-end">
+          <input
+            type="checkbox"
+            checked={selectedMods.find((item) => item === id) !== undefined}
+            onChange={handleChange}
+            className={cn(
+              "h-5 w-5 accent-green-500 border-gray-200 border rounded",
+            )}
+          />
+          <DeleteItemButton
+            clean={true}
+            onClick={() => {
+              removeMods(id);
+              removeModToSelectedList(id);
+            }}
+          />
+        </div>
         <h1 className="font-bold">{title}</h1>
         {description ? <p>{description}</p> : null}
         <div className="flex flex-row gap-1">
